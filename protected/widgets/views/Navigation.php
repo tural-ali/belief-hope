8fddb0549be8c6ccc8b7a63d3ae712bf58dbdf81
Tural Aliyev <turalaliyev@live.com>
2014-01-11 16:32:04 +0000

    social icons

--- protected/widgets/views/Navigation.php
+++ protected/widgets/views/Navigation.php
@@ -3,3 +3,10 @@
         <?= $html ?>
     </ul>
 </div>
+
+<div class="social-bar">
+   <span>Sosial sebekelerde biz:</span>
+   <a class="webicon facebook small" href="#"></a>
+   <a class="webicon twitter small" href="#"></a>
+   <a class="webicon youtube small" href="#"></a>
+</div>
