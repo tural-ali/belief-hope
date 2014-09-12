683a1d6f40a395c53a30d7842df6c0ba2881366d
Tural Aliyev <turalaliyev@live.com>
2014-09-13 00:31:10 +0500

    slider resize

--- protected/views/content/view.php
+++ protected/views/content/view.php
@@ -19,7 +19,7 @@
 
         if (isset($imgUrl))
             if ($catID == 6 || $catID == 7 || is_null($catID))
-                echo "<img class='cover-photo portrait' src='/uploads/images/resize.php?src=$imgUrl&w=2500&zc=2&a=t'  alt=''/>";
+                echo "<img class='cover-photo portrait' src='/uploads/images/resize.php?src=$imgUrl&w=250&zc=2&a=t'  alt=''/>";
             else
                 echo "<center><img class='cover-photo landscape' src='/uploads/images/resize.php?src=$imgUrl&w=650&zc=2&a=t' alt=''/></center>";
 
